26dcaed2a02f1a3bc590e95d7e59a9b973a261b6
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-02-22 16:11:28 +0100

    Přidejte soubory projektu.

---
+++ Ct1450_Obdelnik/Model/Obdelnik.cs
@@ -0,0 +1,30 @@
+﻿namespace Ct1450_Obdelnik.Model
+{
+    public class Obdelnik
+    {
+        public int StranaA { get; set; }
+        public int StranaB { get; set; }
+        public int Obvod { get; set; }
+        public int Obsah { get; set; }
+        public double Uhlopricka { get; set; }
+
+        private void SpoctiObsah()
+        {
+            Obsah = StranaA * StranaB;
+        }
+        private void SpoctiObvod()
+        {
+            Obvod = 2 * (StranaA + StranaB);
+        }
+        private void SpoctiUhlopricka()
+        {
+            Uhlopricka = Math.Sqrt(Math.Pow(StranaA, 2) + StranaB * StranaB);
+        }
+        public void Vypocet()
+        {
+            SpoctiObsah();
+            SpoctiObvod();
+            SpoctiUhlopricka();
+        }
+    }
+}
