cc9e929437dc18c4544b44ba97e27c1951f978f6
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-02-29 15:36:47 +0100

    vypocty pomoci Getter

---
+++ Ct1450_Obdelnik/Model/Obdelnik2.cs
@@ -0,0 +1,30 @@
+﻿namespace Ct1450_Obdelnik.Model
+{
+    public class Obdelnik2
+    {
+        private int stranaA;
+        private int stranaB;
+
+        public int StranaA
+        {
+            get => stranaA;
+            set
+            {
+                if (stranaA != value)
+                {
+                    if(value < 0)
+                        stranaA = 0;
+                    else
+                        stranaA = value;
+                }
+                //Vypocet();
+            }
+        }
+        public int StranaB { get => stranaB; set => stranaB = Math.Abs(value); }
+        public int Obvod => 2 * (StranaA + StranaB);
+        public int Obsah =>StranaA * StranaB;
+        public double Uhlopricka => Math.Sqrt(Math.Pow(StranaA, 2) + StranaB * StranaB);
+
+       
+    }
+}
